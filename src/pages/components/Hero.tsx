d8bcbd762ee96406f3f6ca1c2dc58a37dba47387
ChorokGreen <chorok.lee@gtsolomon.com>
2023-10-19 10:43:27 +0900

    Add components

---
+++ src/pages/components/Hero.tsx
@@ -0,0 +1,7 @@
+import Header from "@/pages/components/Header";
+
+export default function Hero() {
+    return (<div><Header/>
+        <div>banner description</div>
+    </div>)
+}
---
+++ src/pages/components/NetflixOriginals.tsx
@@ -0,0 +1,6 @@
+import Poster from "@/pages/components/Poster";
+
+export default function NetflixOriginals() {
+    return <div><h1>NetflixOriginals</h1><Poster/></div>
+}
+// todo - ssr static image import
